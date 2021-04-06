86d16bd525fac208bc21efa865012d38e6096245
Dennis Cai <dennis@thecodinganalyst.com>
2021-04-06 14:01:24 +0800

    initial commit

---
+++ src/main/java/com/howtographql/hackernews/Link.java
@@ -0,0 +1,27 @@
+package com.howtographql.hackernews;
+
+public class Link {
+
+    private final String id;
+    private final String url;
+    private final String description;
+
+    public Link(String url, String description) {
+        this(null, url, description);
+    }
+
+    public Link(String id, String url, String description) {
+        this.id = id;
+        this.url = url;
+        this.description = description;
+    }
+    public String getUrl() {
+        return url;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getId(){ return id; }
+}
