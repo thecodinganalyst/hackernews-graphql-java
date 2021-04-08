9a425070d6dcb0ecad7785187f2dfa502b8aafb6
Dennis Cai <dennis@thecodinganalyst.com>
2021-04-08 11:22:45 +0800

    added filtering function for getAllLinks

---
+++ src/main/java/com/howtographql/hackernews/LinkFilter.java
@@ -0,0 +1,30 @@
+package com.howtographql.hackernews;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class LinkFilter {
+
+    @JsonProperty("description_contains")
+    public String getDescriptionContains() {
+        return descriptionContains;
+    }
+
+    public void setDescriptionContains(String descriptionContains) {
+        this.descriptionContains = descriptionContains;
+    }
+
+    @JsonProperty("url_contains")
+    public String getUrlContains() {
+        return urlContains;
+    }
+
+    public void setUrlContains(String urlContains) {
+        this.urlContains = urlContains;
+    }
+
+    private String descriptionContains;
+    private String urlContains;
+
+
+
+}
--- src/main/java/com/howtographql/hackernews/LinkRepository.java
+++ src/main/java/com/howtographql/hackernews/LinkRepository.java
@@ -1,12 +1,17 @@
 package com.howtographql.hackernews;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import com.mongodb.client.MongoCollection;
 import org.bson.Document;
+import org.bson.conversions.Bson;
 import org.bson.types.ObjectId;
 
 import static com.mongodb.client.model.Filters.eq;
+import static com.mongodb.client.model.Filters.regex;
+import static com.mongodb.client.model.Filters.and;
+
 
 public class LinkRepository {
 
@@ -21,17 +26,35 @@ public class LinkRepository {
         return link(doc);
     }
 
-    public List<Link> getAllLinks() {
+    public List<Link> getAllLinks(LinkFilter filter) {
+        Optional<Bson> mongoFilter = Optional.ofNullable(filter).map(this::buildFilter);
+
         List<Link> allLinks = new ArrayList<>();
-        for (Document doc : links.find()) {
-            Link link = new Link(
-                    doc.get("_id").toString(),
-                    doc.getString("url"),
-                    doc.getString("description"),
-                    doc.getString("postedBy"));
-            allLinks.add(link);
+        for (Document doc : mongoFilter.map(links::find).orElseGet(links::find)){
+            allLinks.add(link(doc));
         }
         return allLinks;
+    }
+
+    private Bson buildFilter(LinkFilter filter) {
+        String descriptionPattern = filter.getDescriptionContains();
+        String urlPattern = filter.getUrlContains();
+        Bson descriptionCondition = null;
+        Bson urlCondition = null;
+
+        if(descriptionPattern != null && !descriptionPattern.isEmpty()){
+            descriptionCondition = regex("description", ".*" + descriptionPattern + ".*", "i");
+        }
+
+        if(urlPattern != null && !urlPattern.isEmpty()){
+            urlCondition = regex("url", ".*" + urlPattern + ".*", "i");
+        }
+
+        if(descriptionCondition != null && urlCondition != null){
+            return and(descriptionCondition, urlCondition);
+        }
+
+        return descriptionCondition != null ? descriptionCondition : urlCondition;
     }
 
     public void saveLink(Link link) {
